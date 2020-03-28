9855f27daaf92ee0b23be539cd05a7fefe592a0b
zhaody26 <83088341@qq.com>
2020-03-28 22:14:13 +0800

    fix bug

--- Scripts/douban.py
+++ Scripts/douban.py
@@ -16,14 +16,15 @@ r = requests.get('https://movie.douban.com/top250',headers =headers)
 
 # 解析Html，提取数据
 html = etree.HTML(r.text)
-xpath_name = '//*[@id="content"]/div/div[1]/ol/li[1]/div/div[2]/div[1]/a/span[1]'
+xpath_name = '//*[@id="content"]/div/div[1]/ol/li/div/div[2]/div[1]/a/span[1]/text()'
 movie_names = html.xpath(xpath_name)
-print(movie_names)
+# print(movie_names)
 
 
 # 存储数据
 with open(os.path.join(output_dir, 'movies.csv'), 'w') as outfile:
+    outfile.write('movie_name\n')
     for movie_name in movie_names:
-        print(movie_name)
+        # print(movie_name)
         outfile.write(movie_name)
         outfile.write('\n')
